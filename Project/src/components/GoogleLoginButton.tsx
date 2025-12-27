8d845b1b48d54b9070499e698da8f9ad167b2cf9
CHOI SIU <teran030210@gmail.com>
2025-12-28 03:33:29 +0900

    구글로그인 API 수정

--- Project/src/components/GoogleLoginButton.tsx
+++ Project/src/components/GoogleLoginButton.tsx
@@ -1,10 +1,21 @@
-import type { FunctionComponent } from 'react';
+import { type FunctionComponent } from 'react';
 import styles from './GoogleLoginButton.module.css';
 import googleLogo from '../assets/Google.png';
 
 const GoogleLoginButton: FunctionComponent = () => {
+  
+  const handleGoogleLogin = () => {
+    const baseUrl = import.meta.env.VITE_API_BASE_URL;
+    
+    window.location.href = `${baseUrl}/auth/oauth/google`;
+  };
+
   return (
-    <button type="button" className={styles.googleButton}>
+    <button 
+      type="button" 
+      className={styles.googleButton}
+      onClick={handleGoogleLogin}
+    >
       <div className={styles.contentWrapper}>
         <img 
           src={googleLogo} 
